5c3ea3d2a9b3834e928836441325430caadd3c96
Jorge Nascimento <62903240+Jg-Nascimento@users.noreply.github.com>
2021-08-10 10:46:24 -0300

    Update model.py

--- model.py
+++ model.py
@@ -13,15 +13,15 @@ dataset['sales_in_first_month'].fillna(dataset['sales_in_first_month'].mean(), i
 X = dataset.iloc[:, :3]
 
 def convert_to_int(word):
-    word_dict = {'one':1, 'two':2, 'three':3, 'four':4, 'five':5, 'six':6, 'seven':7, 'eight':8,
-                'nine':9, 'ten':10, 'eleven':11, 'twelve':12, 'zero':0, 0: 0}
+    word_dict = {'one':1, 'two':2, 'three':3, 'four':4, 'five':5, 
+                 'six':6, 'seven':7, 'eight':8,
+                 'nine':9, 'ten':10, 'eleven':11, 
+                 'twelve':12, 'zero':0, 0: 0}
     return word_dict[word]
 
 X['rate'] = X['rate'].apply(lambda x : convert_to_int(x))
-
 y = dataset.iloc[:, -1]
 
-from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 
 regressor.fit(X, y)
