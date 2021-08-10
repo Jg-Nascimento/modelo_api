fa30cc0ce09d2f7c31f12225b84f62db9c900a62
Jorge Nascimento <62903240+Jg-Nascimento@users.noreply.github.com>
2021-08-10 10:59:49 -0300

    Update request.py

--- request.py
+++ request.py
@@ -1,6 +1,6 @@
 import requests
 
-url = 'http://0.0.0.0:5000/results'
+url = 'https://modeloapi.herokuapp.com:5000/results'
 r = requests.post(url,json={'rate':5, 'sales_in_first_month':200, 'sales_in_second_month':400})
 
 print(r.json())
